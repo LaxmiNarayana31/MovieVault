d426f13a0295e7111a526c1b050a4d6450841e4d
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-10-31 17:50:43 +0530

    Added TMDB image URLs

--- src/config/config.js
+++ src/config/config.js
@@ -6,10 +6,10 @@ export const img_500 = "https://image.tmdb.org/t/p/w500";
 export const unavailable =
   "https://www.movienewz.com/img/films/poster-holder.jpg";
 
-// contentModal
+// contentModal and singleContent
 export const unavailableLandscape =
   "https://user-images.githubusercontent.com/10515204/56117400-9a911800-5f85-11e9-878b-3f998609a6c8.jpg";
 
-// For Carousel
+// For Carousel and CarouselItem
 export const noPicture =
   "https://upload.wikimedia.org/wikipedia/en/6/60/No_Picture.jpg";
